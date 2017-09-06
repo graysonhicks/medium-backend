5e7a0be9e316aa4ca92c35aa6340477c1bcb8f58
Grayson Hicks <graysonhicks@gmail.com>
2017-09-05 22:08:06 -0400

    json parsing

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.get('/', function (req, res) {
   // hit AWS API gateway to trigger AWS lambda function that parses JSON
   request('https://rqq0gavu79.execute-api.us-west-2.amazonaws.com/Active/medium', function (error, response, body) { // api url
     if (!error && response.statusCode === 200) {
+      body = JSON.parse(body);
       res.send(body); // if no errors, send the body of data back to front end
     }
    });
