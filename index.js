73c31668a22731de0f7dc8e8a6fcbdad7885d423
Grayson Hicks <graysonhicks@gmail.com>
2017-09-15 20:43:00 -0400

    trying none lambda

--- index.js
+++ index.js
@@ -7,14 +7,40 @@ var cors = require('cors');
 app.use(cors()); //allows overriding cross origin policy (use npm install if needed)
 
 app.get('/', function (req, res) {
+  var username = 'graysonhicks'
+	var url = "https://medium.com/@"+username+"/latest?format=json";
   // hit AWS API gateway to trigger AWS lambda function that parses JSON
-  request('https://rqq0gavu79.execute-api.us-west-2.amazonaws.com/Active/medium', function (error, response, body) { // api url
+  request({url: url, json: true}, function (error, response, body) { // api url
     if (!error && response.statusCode === 200) {
+      	// We need to pull out the text that comes thru from Medium for JSON hacking
+    		jsonBody = JSON.parse(body.replace('])}while(1);</x>', ''));
       body = JSON.parse(body);
       res.send(body); // if no errors, send the body of data back to front end
     }
    });
 });
+// 
+// var request = require("request"); // Include the request lib - run npm install request
+// exports.handler = function(event, context, callback) {
+// 	callback = context.done;
+// 	var data = event.bodyJson || {};
+//
+// 	// Sets the username - replace username with your Medium Username
+//
+//
+// 	// Make the request to Medium and return the Obj
+// 	request({
+// 	    url: url,
+// 	    json: true
+// 	}, function (error, response, body) {
+// 	    if (!error && response.statusCode === 200) {
+//
+// 	        console.log(jsonBody) // Print the json response
+// 	        callback(null, jsonBody); // Return the JSON object back to our API call
+// 	    }
+// 	});
+// };
+
 
 
 var port = process.env.PORT || 3000;
